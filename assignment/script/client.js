1e303147ff9644231f3af49b6eca44af7b209991
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-09 21:29:28 -0500

    input values store in object array

--- assignment/script/client.js
+++ assignment/script/client.js
@@ -1,7 +1,25 @@
-console.log('connected');
+const employeeList = [];
 
 $(document).ready(onReady);
 
 function onReady() {
-  console.log('jquery connected');
+  $('.js-submitButton').on('click', submitValues);
+}
+
+function submitValues() {
+  let firstName = $('.js-firstNameInput').val();
+  let lastName = $('.js-lastNameInput').val();
+  let id = $('.js-idInput').val();
+  let job = $('.js-jobInput').val();
+  let salary = $('.js-salaryInput').val();
+
+  const employee = {
+    firstName,
+    lastName,
+    id,
+    job,
+    salary,
+  };
+  employeeList.push(employee);
+  console.log(employee);
 }
