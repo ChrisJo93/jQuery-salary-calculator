9a0333fac03c6a092599bf17c1495c4db8829bf1
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-10 14:09:14 -0500

    created display function with working loop.

--- assignment/script/client.js
+++ assignment/script/client.js
@@ -1,18 +1,20 @@
-const employeeList = [];
+const employeeArray = [];
 
 $(document).ready(onReady);
 
 function onReady() {
-  $('.js-submitButton').on('click', submitValues);
+  $('.js-submitButton').on('click', submitValues); //"connects button with input values function
 }
 
 function submitValues() {
+  //targeting input fields and storing those values
   let firstName = $('.js-firstNameInput').val();
   let lastName = $('.js-lastNameInput').val();
   let id = $('.js-idInput').val();
   let job = $('.js-jobInput').val();
   let salary = $('.js-salaryInput').val();
 
+  //using the input field values to build employee object
   const employee = {
     firstName,
     lastName,
@@ -20,6 +22,13 @@ function submitValues() {
     job,
     salary,
   };
-  employeeList.push(employee);
-  console.log(employee);
+  employeeArray.push(employee); //storing employee object into an array
+  employeeDisplay();
+}
+
+function employeeDisplay() {
+  $('.js-employeeDisplayList').empty();
+  for (let i = 0; i < employeeArray.length; i++) {
+    console.log(employeeArray[i]);
+  }
 }
